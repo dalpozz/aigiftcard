5b74f09be7cf74251d1f1ba71aea146852ffc038
dalpozza <andrea.dal_pozzolo@roche.com>
2023-11-03 22:08:22 +0100

    pager

--- card/main.py
+++ card/main.py
@@ -1,14 +1,30 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
+stripe_js = """<script async
+  src="https://js.stripe.com/v3/buy-button.js">
+</script>
+
+<stripe-buy-button
+  buy-button-id="buy_btn_1O6spGAWMFUrkCpQokDCuxQR"
+  publishable-key="{}"
+>
+</stripe-buy-button>
+""".format(st.secrets["stripe_publishable_key"])
+
+
+
 def main():
     st.title("AI Gift Card")
-    st.write("Welcome to the AI Gift Card app!")
+    st.subheader("Welcome to the AI Gift Card app!")
     st.write("Enter your gift card code below:")
     code = st.text_input("Gift card code")
     if st.button("Check balance"):
         # Here you can add code to check the balance of the gift card
         st.write(f"The balance of gift card {code} is $50")
+
+    #components.html(html=stripe_js, height=300)
+
 
 if __name__ == "__main__":
     main()
